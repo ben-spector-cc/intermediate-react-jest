2402abad7833098a37effab6bad95102aa0a0bb5
athegrayt <68484756+athegrayt@users.noreply.github.com>
2021-06-03 18:24:50 -0500

    Update axios.js

--- project/currency_comparison_final/__mocks__/axios.js
+++ project/currency_comparison_final/__mocks__/axios.js
@@ -1,7 +1,7 @@
 module.exports = {
     get: jest.fn(() => {
         return Promise.resolve({
-            status : "",
+            status : "Mock",
             data: {}
             })
         })
