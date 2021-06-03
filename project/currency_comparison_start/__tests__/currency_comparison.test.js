f5c6628c6f0e3d290e11dcd34a88a82b6359da08
athegrayt <68484756+athegrayt@users.noreply.github.com>
2021-06-03 18:17:52 -0500

    Update currency_comparison.test.js
    Changes made based on first draft critiques.

--- project/currency_comparison_start/__tests__/currency_comparison.test.js
+++ project/currency_comparison_start/__tests__/currency_comparison.test.js
@@ -2,34 +2,23 @@ const CurrencyComparison = require('../currency_comparison')
 
 const testSalary = new CurrencyComparison(50000)
     
-test("Converts foreign currency to USD", () => {
-    //arrange
-    const currencyCode1 = 'CAD'
-    const expectedValue1 = .82
-    const currencyCode2 = 'EUR'
-    const expectedValue2 = 1.21
-    const rates = {
-            "MXN": 24.1677,
-            "CAD": 1.472,
-            "USD": 1.2143
-            }
-    //act
-    const actualValue1 = testSalary.currencyConversion(rates, currencyCode1) 
-    const actualValue2 = testSalary.currencyConversion(rates, currencyCode2) 
-    //assertions
-    
-    
-})
+//Place test for testSalary.currencyConversion below
 
-test("Convert foreign currency into USD equivalent hourly pay", () => {
-    //arrange
-    const exchangeRate = 1.21
-    const expectedValue = 30.25
-    //act
-    const actualValue = testSalary.hourlyPayUSD(exchangeRate)
-    //assertions
+
+
+
+
+
+
+//Place test for testSalary.hourlyPayUSD below
     
-})
+
+
+
+
+
+
+
 test("respond with different salaries based on currency", () => {
     //arrange
     const currency = "EUR"
@@ -59,6 +48,8 @@ test("Receives current currency exchange data", ()=>{
     //testing for REST API status code
 })
 
+
+
 // test("Receives current currency exchange data", async ()=>{
 //     //arrange
 //     const response = {
@@ -73,7 +64,7 @@ test("Receives current currency exchange data", ()=>{
 //                 "date": "2021-05-17"
 //             }
 //     }
-//     axios.get.mockResolvedValueOnce(response)
+//     
 //     //act
 //     const actualValue = await testSalary.fetchCurrentExchange() 
 //     console.log(actualValue);
